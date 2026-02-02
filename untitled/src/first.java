65564eae060c71f496e23ab7549e07f461d9646e
Maawiya06 <maawiyaameer001@gmail.com>
2026-02-02 21:04:34 +0530

    commit

--- untitled/src/first.java
+++ untitled/src/first.java
@@ -3,5 +3,9 @@ public class first {
 
     public static void main(String[] args){
         System.out.println("Hello world");
+
+        // crate variable
+        int a = 4;
+        System.out.println(a);
     }
 }
