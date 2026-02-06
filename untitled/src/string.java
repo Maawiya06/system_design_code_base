172a1ce0d194983f2d57a78cad128d900007f1ca
Maawiya06 <maawiyaameer001@gmail.com>
2026-02-07 00:17:27 +0530

    commit

--- untitled/src/string.java
+++ untitled/src/string.java
@@ -2,5 +2,9 @@ public class string {
 
     static void main() {
         System.out.println("This is For String");
+        String name = "Ameer";
+        System.out.print(name + " ");
+        String name1 = new String("Maawiya");
+        System.out.print(name1);
     }
 }
