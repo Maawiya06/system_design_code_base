18beb24228fbb3115aba2fd6c4623a51ce43c12b
Maawiya06 <maawiyaameer001@gmail.com>
2026-02-07 00:43:04 +0530

    commit

--- untitled/src/string.java
+++ untitled/src/string.java
@@ -11,5 +11,12 @@ public class string {
         StringBuilder str = new StringBuilder("Saifullah");
         str.setCharAt(1, 'L');
         System.out.println(str);
+
+        // split the string
+        String str1 = "All is good here and i also";
+        String[] words = str1.split(" ");
+        for(String word : words){
+            System.out.println(word);
+        }
     }
 }
