644faf896d97b81ad4026675615bc7bc54ff7310
Maawiya06 <maawiyaameer001@gmail.com>
2026-02-09 00:45:33 +0530

    commit

--- untitled/src/OOPs/ObjectCreation.java
+++ untitled/src/OOPs/ObjectCreation.java
@@ -8,6 +8,16 @@ public class ObjectCreation {
     public String name;
     public int nos;
 
+    // behaviour
+    public void study() {
+        System.out.println(name + "studying");
+    }
 
+    public void sleep(){
+        System.out.println(name + "sleeping");
+    }
 
+    public void bunk(){
+        System.out.println(name + "Bunking");
+    }
 }
