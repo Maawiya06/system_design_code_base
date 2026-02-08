283f1307e4871ff8272287e37783f125a111ce22
Maawiya06 <maawiyaameer001@gmail.com>
2026-02-09 00:55:22 +0530

    commit

--- untitled/src/OOPs/App.java
+++ untitled/src/OOPs/App.java
@@ -3,6 +3,18 @@ package OOPs;
 public class App {
     static void main(String[] args) {
         ObjectCreation A = new ObjectCreation();
+        A.id = 1;
+        A.age = 23;
+        A.name = "Rannu";
+        A.nos = 5;
         System.out.println(A.name);
+        System.out.println(A.age);
+        System.out.println(A.id);
+        System.out.println(A.nos);
+
+        // methods call
+        A.bunk();
+        A.sleep();
+        A.study();
     }
 }
