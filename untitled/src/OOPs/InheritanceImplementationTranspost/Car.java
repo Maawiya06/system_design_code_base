ac765c915cf3ec6810ae6dd707abd15666652409
Maawiya06 <maawiyaameer001@gmail.com>
2026-02-10 22:05:41 +0530

    commit

--- untitled/src/OOPs/InheritanceImplementationTranspost/Car.java
+++ untitled/src/OOPs/InheritanceImplementationTranspost/Car.java
@@ -1,4 +1,19 @@
 package OOPs.InheritanceImplementationTranspost;
 
+import java.sql.SQLOutput;
+
 public class Car extends vehicle{
+
+    public int noOfDoors;
+    public String transmissinType;
+
+    Car(int noOfDoors, String transmissinType, String name, String model, int noOfTyres){
+        super(name, model, noOfTyres);
+        this.noOfDoors = noOfDoors;
+        this.transmissinType = transmissinType;
+    }
+
+    void startAc(){
+        System.out.println("Ac is Starting in the car : " + name);
+    }
 }
