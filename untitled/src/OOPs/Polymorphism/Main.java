2a6617d943b3e5109cbd4cf034ac3f5cadf42987
Maawiya06 <maawiyaameer001@gmail.com>
2026-02-11 12:56:54 +0530

    commit

---
+++ untitled/src/OOPs/Polymorphism/Main.java
@@ -0,0 +1,8 @@
+package OOPs.Polymorphism;
+
+public class Main {
+    static void main(String[] args) {
+        Calculator c = new Calculator();
+        System.out.println(c.add(3, 6));
+    }
+}
