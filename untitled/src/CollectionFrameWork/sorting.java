fd3f19db87d0f01fdbff8844b3a7884b9ad1d62c
Maawiya06 <maawiyaameer001@gmail.com>
2026-02-12 21:28:27 +0530

    commit

--- untitled/src/CollectionFrameWork/sorting.java
+++ untitled/src/CollectionFrameWork/sorting.java
@@ -14,20 +14,25 @@ public class sorting {
         ans.add(600);
         ans.add(1);
 
+        Collections.reverse(ans);
+        System.out.println("reverse order : " + ans);
+
         System.out.println("Before Sorting: " + ans);
         Collections.sort(ans);
         System.out.println("After Sorting: " + ans);
 
+        Collections.reverse(ans);
+        System.out.println(ans);
         // sorting in decending order
-        Collections.sort(ans, new Comparator<Integer>() {
-            @Override
-            public int compare(Integer o1, Integer o2) {
-                return o2.compareTo(o1);
-            }
-        });
-
-        Collections.sort(ans, (o1, o2)-> o2.compareTo(o1));
-        Collections.sort(ans, Collections.reverseOrder());
-        System.out.println("Sorted element in descendng order: " + ans);
+//        Collections.sort(ans, new Comparator<Integer>() {
+//            @Override
+//            public int compare(Integer o1, Integer o2) {
+//                return o2.compareTo(o1);
+//            }
+//        });
+//
+//        Collections.sort(ans, (o1, o2)-> o2.compareTo(o1));
+//        Collections.sort(ans, Collections.reverseOrder());
+//        System.out.println("Sorted element in descendng order: " + ans);
     }
 }
